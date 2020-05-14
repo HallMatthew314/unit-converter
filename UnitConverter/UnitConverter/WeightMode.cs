5fbd76d32f03e9b1eecc73856f9e7285c4f17b17
Matthew Hall <hallmg1@student.op.ac.nz>
2020-05-14 17:08:31 +1200

    Weights con be converted

--- UnitConverter/UnitConverter/WeightMode.cs
+++ UnitConverter/UnitConverter/WeightMode.cs
@@ -15,7 +15,39 @@ namespace UnitConverter
 
     public class WeightMode : Mode
     {
-        public WeightMode(Form1 _host) : base(_host){}
+        public WeightMode(Form1 _host) : base(_host)
+        {
+            conversions = new Dictionary<int, Dictionary<int, Func<double, double>>>()
+            {
+                // base unit grams
+                {
+                    (int)EWeights.Grams, new Dictionary<int, Func<double, double>>()
+                    {
+                        { (int)EWeights.Grams, (x) => x },
+                        { (int)EWeights.Pounds, (x) => ConversionMethods.gTolb(x) },
+                        { (int)EWeights.Stone, (x) => ConversionMethods.gToStone(x) }
+                    }
+                },
+                // base unit pounds
+                {
+                    (int)EWeights.Pounds, new Dictionary<int, Func<double, double>>()
+                    {
+                        { (int)EWeights.Grams, (x) => ConversionMethods.lbTog(x) },
+                        { (int)EWeights.Pounds, (x) => x },
+                        { (int)EWeights.Stone, (x) => ConversionMethods.lbToStone(x) }
+                    }
+                },
+                // base unit stone
+                {
+                    (int)EWeights.Stone, new Dictionary<int, Func<double, double>>()
+                    {
+                        { (int)EWeights.Grams, (x) => ConversionMethods.stoneTog(x) },
+                        { (int)EWeights.Pounds, (x) => ConversionMethods.stoneTolb(x) },
+                        { (int)EWeights.Stone, (x) => x }
+                    }
+                }
+            };
+        }
 
         public override Array UnitValues
         {
