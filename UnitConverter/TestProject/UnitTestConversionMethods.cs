f8a3c98a46ac8691e1143d7de19684e937f2006c
Matthew Hall <hallmg1@student.op.ac.nz>
2020-06-10 11:48:49 +1200

    Create and link testing project

---
+++ UnitConverter/TestProject/UnitTestConversionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitConverter;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTestConversionMethods
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            // placeholder code
+            UnitConverter.ConversionMethods.mmToCm(1.0);
+            Assert.Fail();
+        }
+    }
+}
